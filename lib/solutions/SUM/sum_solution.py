240a36f34d202c47635ff165305a9dc9e2ec6743
sbx_user1051 <sbx_user1051@169.254.118.25>
2022-04-25 08:40:28 +0000

    Mon Apr 25 08:40:28 UTC 2022

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,11 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
+    if x < 0 or y < 0:
+        raise ValueError("Expect positive values")
+    if x > 100 or y > 100:
+        raise ValueError("inputs cannot be more than 100")
+    if not (isinstance(x, int) and isinstance(y, int)):
+        raise ValueError('inputs must be integer')
     return sum((x, y))
+
 
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -6,3 +6,8 @@ class TestSum(TestCase):
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
 
+    def test_raise_error_if_less_than_zero(self):
+        with ValueError:
+            sum_solution.compute(-1, 2)
+
+
