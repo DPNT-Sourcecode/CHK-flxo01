215272298ff61d0068c91206134b4f809a4550b8
sbx_user1051 <sbx_user1051@169.254.118.25>
2022-04-25 09:41:29 +0000

    Mon Apr 25 09:41:29 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -11,9 +11,17 @@ class TestCheckout(TestCase):
         assert checkout_solution.checkout('AAA') == 130
         assert checkout_solution.checkout('AAAA') == 130 + 50
         assert checkout_solution.checkout('AAAAA') == 200
+        assert checkout_solution.checkout('AAAAAAAAA') == 200 + 130 + 50
         assert checkout_solution.checkout('BB') == 45
         assert checkout_solution.checkout('BBB') == 45 + 30
         assert checkout_solution.checkout('CC') == 40
         assert checkout_solution.checkout('DD') == 30
         assert checkout_solution.checkout('ABCDABA') == 175 + 35
+        assert checkout_solution.checkout('E') == 40
+        assert checkout_solution.checkout('EEB') == 80
+        assert checkout_solution.checkout('EEBB') == 80 + 30
+        assert checkout_solution.checkout('EEEB') == 120
+        assert checkout_solution.checkout('EEBBB') == 80 + 45
+
+
 
