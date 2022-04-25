2ed563c1c4277c5391e04849a9e9ef5fc25808fc
sbx_user1051 <sbx_user1051@169.254.118.25>
2022-04-25 09:08:47 +0000

    Mon Apr 25 09:08:47 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,5 +2,21 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+def get_price(units, item):
+    if item not in ['A', 'B', 'C', 'D']:
+        return -1
+    if not isinstance(units, int):
+        raise TypeError('units must be int')
+    if item == 'C':
+        return units * 20
+    elif item == 'D':
+        return units * 15
+    elif item == 'A':
+        rounded_items = units / 3
+        return rounded_items * 130 + (units - rounded_items*3)*50
+
 def checkout(skus):
-    raise NotImplementedError()
+
+
+
