8d63d58ca94d60ff04ad8cd2a9b4875c5879499e
sbx_user1051 <sbx_user1051@169.254.118.25>
2022-04-25 08:43:28 +0000

    Mon Apr 25 08:43:28 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -5,9 +5,19 @@ from unittest import TestCase
 class TestSum(TestCase):
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+        assert sum_solution.compute(0, 100) == 100
 
     def test_raise_error_if_less_than_zero(self):
-        with ValueError:
+        with self.assertRaises(ValueError):
             sum_solution.compute(-1, 2)
+
+    def test_raise_error_if_more_than_one_hundred(self):
+        with self.assertRaises(ValueError):
+            sum_solution.compute(0, 101)
+
+    def test_raise_error_if_not_integer(self):
+        with self.assertRaises(ValueError):
+            sum_solution.compute(0.5, 10)
+
 
 
