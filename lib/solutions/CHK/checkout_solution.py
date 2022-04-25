c70025cd740dccbea4c7fff56f5d4da15fe477b6
sbx_user1051 <sbx_user1051@169.254.118.25>
2022-04-25 09:52:37 +0000

    Mon Apr 25 09:52:37 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,26 +3,31 @@ from math import floor
 # noinspection PyUnusedLocal
 # skus = unicode string
 
+UNIT_PRICE = {
+    'A': 50,
+    "B": 30,
+    'C': 20,
+    'D': 15,
+    'E': 40,
+    'F': 10
+}
+
 
 def get_price_no_free(units, item):
     if not isinstance(units, int):
         raise TypeError('units must be int')
-    if item == 'C':
-        return units * 20
-    elif item == 'D':
-        return units * 15
+    unit_price = UNIT_PRICE[item]
+    if item in ['C', 'D', 'E', 'F']:
+        return unit_price * units
     elif item == 'A':
         rounded_5_items = floor(units / 5)
         rounded_3_items = floor((units - rounded_5_items * 5) / 3)
-        return rounded_5_items * 200 + rounded_3_items * 130 + (units - rounded_5_items * 5 - rounded_3_items * 3) * 50
+        return rounded_5_items * 200 + rounded_3_items * 130 + \
+               (units - rounded_5_items * 5 - rounded_3_items * 3) * unit_price
     elif item == 'B':
         rounded_items = floor(units / 2)
-        return rounded_items * 45 + (units - rounded_items * 2) * 30
-    elif item == 'E':
-        return units * 40
-    elif item == 'F':
-        return units * 10
-    
+        return rounded_items * 45 + (units - rounded_items * 2) * unit_price
+
 
 def get_price(units, item, free_units_dict):
     if units == 0:
@@ -50,5 +55,6 @@ def checkout(skus):
             sku_count[i] += 1
     free_units_dict = get_free_items(sku_count)
     return sum([get_price(units, item, free_units_dict) for item, units in sku_count.items()])
+
 
 
