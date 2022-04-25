ff1b7d235b103b526b1af9f6796e5bced83a8c0b
sbx_user1051 <sbx_user1051@169.254.118.25>
2022-04-25 08:34:28 +0000

    Mon Apr 25 08:34:28 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,6 +1,8 @@
-from solutions.SUM import sum_solution
+from lib.solutions.SUM import sum_solution
+from unittest import TestCase
 
 
-class TestSum():
+class TestSum(TestCase):
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
